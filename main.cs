7756b859743a7c8843c2e7c9661f043b955d6a47
A Wonkey Tortila <74030195+AWonkeyTortila@users.noreply.github.com>
2022-03-08 21:11:50 -0800

    Added basic "Hello World!" statement

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+  
+namespace HelloWorldApp {
+      
+    class program {
+          
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
