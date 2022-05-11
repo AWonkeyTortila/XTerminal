23e58c8e5c8eaa4fedd3b77d05f6c79606b9141a
Wonkey <TortilaBoi@gmail.com>
2022-05-11 14:46:07 -0700

    Testing

---
+++ main.c
@@ -0,0 +1,18 @@
+// Made by Wonkey, 5/10/22 at 9:48 PM (PST)
+
+#include <stdio.h>
+
+// please make sure the string system works LMAO.
+void main(){
+
+	printf("Hello, my name is Emily Chan! What's yours?\n");
+	/* And to adress the infamous comment,
+	no, Emily Chan is not bisexual, bruh. */
+	char userName[100];
+
+	scanf("%s", userName);
+
+	printf("Hello, %s", userName);
+
+	getchar();
+}
